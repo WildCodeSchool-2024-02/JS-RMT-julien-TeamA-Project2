4ecaab73ea08d31664f3512177a0db3365d42c1f
BastienDmr <domer.bastien@gmail.com>
2024-05-22 12:04:32 +0200

    Add route movies and insert data

--- server/app/router.js
+++ server/app/router.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const movies = require("../database/data");
+
 const router = express.Router();
 
 /* ************************************************************************* */
@@ -10,7 +12,9 @@ const router = express.Router();
 const itemActions = require("./controllers/itemActions");
 
 // Route to get a list of items
-router.get("/items", itemActions.browse);
+router.get("/movies", (req, res) => {
+  res.status(200).json(movies);
+});
 
 // Route to get a specific item by ID
 router.get("/items/:id", itemActions.read);
