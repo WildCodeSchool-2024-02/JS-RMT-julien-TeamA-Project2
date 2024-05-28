185bf1dad3a12a9056d65102b8786f056881ae6a
Mkathalina Sanchez <mkathalina.sanchez@gmail.com>
2024-05-28 17:58:35 +0200

    add responsive style

--- client/src/components/LineMovieItem.jsx
+++ client/src/components/LineMovieItem.jsx
@@ -1,34 +1,36 @@
 import * as PropTypes from "prop-types";
-import styles from "../assets/styles/components/line-movie-item.module.css"
+import styles from "../assets/styles/components/line-movie-item.module.css";
 
-function LineMovieItem({title, posterPath, voteAverage, voteCount}) {
+function LineMovieItem({ title, posterPath, voteAverage, voteCount }) {
   return (
     <div className={styles.lineMovieItem}>
       <div className={styles.lineMovieItemLeft}>
-        <img src={posterPath} alt={title} className={styles.lineMoviePoster}/>
+        <img src={posterPath} alt={title} className={styles.lineMoviePoster} />
       </div>
-      <div className={styles.lineMovieItemRight}>
+      <div className={styles.lineMovieItemCenter}>
         <h2>{title}</h2>
         <div className={styles.lineMovieItemInfo}>
           <div>
-            <span>{voteAverage}</span>
+            <span>{voteAverage}/10</span>
           </div>
-         <div>
-           <span>{voteCount}</span>
-         </div>
+          <div className={styles.voteCount}>
+            <span>{voteCount}</span>
+            <p>Votes</p>
+          </div>
         </div>
       </div>
-
+      <div className={styles.lineMovieItemRight}>
+        <button type="button">❤️</button>
+      </div>
     </div>
-  )
+  );
 }
-
 
 LineMovieItem.propTypes = {
   title: PropTypes.string.isRequired,
   posterPath: PropTypes.string.isRequired,
   voteAverage: PropTypes.number.isRequired,
   voteCount: PropTypes.number.isRequired,
-}
+};
 
-export default LineMovieItem
+export default LineMovieItem;
