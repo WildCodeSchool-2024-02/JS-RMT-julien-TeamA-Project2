95a27e932794e049ce7d1c44b4d4480748be2894
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-05-28 11:38:22 +0200

    import context on App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
+import { useFav } from "./contexts/FavContext";
+
 import "./App.css";
 
 function App() {
+  const { fav, setFav } = useFav();
+  console.info(fav, setFav);
   return <h1>WildersMoviesClub</h1>;
 }
 
