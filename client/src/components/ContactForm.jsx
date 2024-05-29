cc9766066a8d681bba3fbeea1889c54b3657257a
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-05-29 10:41:14 +0200

    stock form infomations in a state

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,25 +1,47 @@
+import { useState } from "react";
 import "../assets/styles/ContactForm.css"
 
 function ContactForm() {
+  const [category, setCategory] = useState("")
+  const [page, setPage] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+  const handleCategory = (event) => {
+    setCategory (event.target.value);
+  }
+  const handlePage = (event) => {
+    setPage (event.target.value);
+  }
+  const handleEmail = (event) => {
+    setEmail (event.target.value);
+  }
+  const handleMessage = (event) => {
+    setMessage (event.target.value);
+  }
+
   return (
     <div className="ContactContainer">
       <h2>Contact Us</h2>
       <p>We're sorry you encounter a problem. Please as detail as possible, we'll correct it as soon as possible</p>
-      <form action="">
-        <label htmlFor="reason"> Select a reason </label>
-        <select className="formEntry" name="reason" id="reason">
-          <option value="">-- Please choose an option --</option>
+      <form action={handleSubmit}>
+        <label htmlFor="category"> Select a reason </label>
+        <select className="formEntry" name="category" id="category" onChange={handleCategory}>
+          <option value={category}>-- Please choose an option --</option>
           <option value="missingMovie"> I can't find one movie</option>
           <option value="mistakeMovie"> I found a mistake about one movie</option>
           <option value="other"> I encounter another problem</option>
         </select>
         <label htmlFor="page">Concerned page</label>
-        <input className="formEntry" type="text" name="page" id="page" />
+        <input className="formEntry" type="text" name="page" id="page" value={page} onChange={handlePage}/>
         <label htmlFor="email">Email</label>
-        <input className="formEntry" type="email" name="email" id="email" />
+        <input className="formEntry" type="email" name="email" id="email" value={email} onChange={handleEmail}/>
         <label htmlFor="message">Message</label>
-        <textarea className="formEntry" name="message" id="message"/>
-        <button type="submit">Send</button>
+        <textarea className="formEntry" name="message" id="message" value={message} onChange={handleMessage}/>
+        <button type="submit" onClick={handleSubmit}>Send</button>
       </form>
     </div>
   )
