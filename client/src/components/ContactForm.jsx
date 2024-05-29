0c70c0ca40db2311731aa6eecbd1d2356b386cb6
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-05-29 16:20:45 +0200

    Display information success or error at submit + reinit form

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -7,6 +7,7 @@ function ContactForm() {
   const [page, setPage] = useState("")
   const [email, setEmail] = useState("")
   const [message, setMessage] = useState("")
+  const [status, setStatus] = useState("")
   const contactForm = useRef();
   
   const handleSubmit = (event) => {
@@ -16,6 +17,18 @@ function ContactForm() {
       .sendForm('service_it7e70d', 'template_k6t4o8x', contactForm.current, {
         publicKey: 'HL6pDegxlrhASDrSs'
       })
+      .then(
+        () => {
+          setStatus("success");
+          setCategory("")
+          setEmail("")
+          setMessage("")
+          setPage("")
+        },
+        () => {
+          setStatus("error")
+        }
+      )
   }
   const handleCategory = (event) => {
     setCategory (event.target.value);
@@ -45,11 +58,13 @@ function ContactForm() {
         <label htmlFor="page">Concerned page</label>
         <input className="formEntry" type="text" name="page" id="page" value={page} onChange={handlePage}/>
         <label htmlFor="email">Email</label>
-        <input className="formEntry" type="email" name="email" id="email" value={email} onChange={handleEmail}/>
+        <input className="formEntry" required type="email" name="email" id="email" value={email} onChange={handleEmail}/>
         <label htmlFor="message">Message</label>
-        <textarea className="formEntry" name="message" id="message" value={message} onChange={handleMessage}/>
+        <textarea className="formEntry" required name="message" id="message" value={message} onChange={handleMessage}/>
         <button type="submit" onClick={handleSubmit}>Send</button>
       </form>
+      {status === "success" && <p className="successMessage">Your message has been sent successfully!</p>}
+      {status === "error" && <p className="errorMessage">An error has occurred. Please try again later.</p>}
     </div>
   )
 }
