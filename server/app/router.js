bd723c57097260d988337065a11787be8c3cfe13
Mkathalina Sanchez <mkathalina.sanchez@gmail.com>
2024-05-30 20:53:03 +0200

    add route get a movie.

--- server/app/router.js
+++ server/app/router.js
@@ -10,14 +10,27 @@ const router = express.Router();
 // Import itemActions module for handling item-related operations
 
 // Route to get a list of items
-router.get("/", (req, res) => {
-  client.query('SELECT * FROM movies')
-  .then((movies) => {
+router.get("/movies", (req, res) => {
+  client.query("SELECT * FROM movies").then((movies) => {
     res.status(200).json(movies[0]);
   });
 });
 
 // Route to get a specific item by ID
+router.get("/movies/:id", (req, res) => {
+  const movieId = req.params.id;
+  client
+    .query("SELECT * FROM movies WHERE id = ? LIMIT 1", [movieId])
+    .then((movies) => {
+      const movie = movies[0][0];
+
+      if (!movie) {
+        res.sendStatus(404);
+      }
+
+      res.status(200).json(movie);
+    });
+});
 
 // Route to add a new item
 
