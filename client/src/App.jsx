b23ef6d539da90caed2ba56fc50f2c505a82c3d5
Mkathalina Sanchez <mkathalina.sanchez@gmail.com>
2024-05-31 13:58:56 +0200

    add function search component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from "react-router-dom";
-
+import { useState } from "react";
 import MovieCard from "./components/MovieCard";
 import SearchBar from "./components/SearchBar";
 import Footer from "./components/Footer";
@@ -8,14 +8,24 @@ import "./App.css";
 
 function App() {
   const movies = useLoaderData();
+  const [search, setSearch] = useState("");
   return (
     <>
       <h1>WildersMoviesClub</h1>
-      <SearchBar />
+      <SearchBar
+        setSearch={setSearch}
+        randomId={movies[Math.floor(Math.random() * movies.length)].id}
+      />
       <section className="app-container">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {movies
+          .filter(
+            (movie) =>
+              search === "" ||
+              movie.title.toLowerCase().indexOf(search.toLowerCase()) !== -1
+          )
+          .map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
       </section>
       <Footer />
     </>
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,12 +1,30 @@
 import "../assets/styles/components/SearchBar.css";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
-function SearchBar() {
+function SearchBar({ setSearch, randomId }) {
+  const navigate = useNavigate();
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value.toLowerCase());
+  };
+  const handleLuckyClick = () => {
+    navigate(`/movies/${randomId}`);
+  };
+
   return (
     <form className="searchBar" onSubmit={(event) => event.preventDefault()}>
-      <input type="text" name="search" />
-      <button type="button">I'm feeling lucky</button>
+      <input type="text" name="search" onChange={handleSearchChange} />
+      <button type="button" onClick={handleLuckyClick}>
+        I'm feeling lucky
+      </button>
     </form>
   );
 }
+
+SearchBar.propTypes = {
+  setSearch: PropTypes.func.isRequired,
+  randomId: PropTypes.number.isRequired,
+};
 
 export default SearchBar;
