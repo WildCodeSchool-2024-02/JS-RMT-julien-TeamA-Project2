9534168d3df4d6bc7790a4db2f5927e72693c0bf
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-02 17:41:48 +0200

    move title on black background, use MoviePoster componant for whole upper part

--- client/src/pages/Moviepage.jsx
+++ client/src/pages/Moviepage.jsx
@@ -9,35 +9,12 @@ function Moviepage() {
   const movie = useLoaderData();
 
   return (
-    <>
-      <section
-        className="moviepage-container"
-        style={{ backgroundImage: `url(${movie.backdrop_path})` }}
-      >
-        <div className="bandeau-container">
-          <MoviePoster key={movie.id} poster={movie} />
-          <section className="bandeau">
-            <div className="bandeau-noinfo" />
-            <div className="bandeau-info">
-              <div>
-                <h1 className="bandeau-title">{movie.title}</h1>
-                <p>{movie.release_date.slice(0, 4)}</p>
-              </div>
-              <div className="bandeau-bg">
-                <p>{movie.genre_ids}</p>
-              </div>
-            </div>
-
-            <div className="bandeau-info">
-              <div />
-              <div className="bandeau-bg bandeau-note">
-                <p>{movie.vote_average}/10</p>
-                <p className="tiny-votecount">{movie.vote_count} votes</p>
-              </div>
-            </div>
-          </section>
-        </div>
-      </section>
+    <>     
+      <MoviePoster key={movie.id} poster={movie} /> 
+      <div className="bandeau-title">
+        <h1>{movie.title}</h1>
+        <p>{movie.release_date.slice(0, 4)}</p>
+      </div>
       <MovieDescription key={movie.id} movie={movie} />
       <Footer />
     </>
