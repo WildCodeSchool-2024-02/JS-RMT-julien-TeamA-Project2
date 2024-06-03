2d51acf8117782e31ed8ae674cbeeffb8362dc86
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-03 17:03:04 +0200

    insert carousel

--- client/src/components/GenderCarousel.jsx
+++ client/src/components/GenderCarousel.jsx
@@ -1,7 +1,30 @@
-function GenderCarousel() {
+import PropTypes from "prop-types";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+
+function GenderCarousel({similarMovies}) {
+  const items = similarMovies.map((movie) => ( <img src={movie.poster_path} alt={movie.title} key={movie.poster_path}/> ));
+  const responsive = {
+    0: { items: 1 },
+    568: { items: 2 },
+    1024: { items: 3 },
+};
   return(
-    <h1>Bientôt un carrousel ici</h1>
+      <AliceCarousel
+          mouseTracking
+          items={items}
+          responsive={responsive}
+          controlsStrategy="alternate"
+      />
   )
+}
+
+GenderCarousel.propTypes = {
+  similarMovies : PropTypes.arrayOf(PropTypes.shape({
+    poster_path : PropTypes.string.isRequired,
+    title : PropTypes.string.isRequired
+  })).isRequired
 }
 
 export default GenderCarousel;
