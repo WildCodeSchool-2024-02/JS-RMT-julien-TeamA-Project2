84e71cedaf55c11f9427d346c45914b74046ad57
BastienDmr <domer.bastien@gmail.com>
2024-06-04 11:05:40 +0200

    Ajout du link et de la proptypes id

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
-
+import { Link } from "react-router-dom";
 import "../assets/styles/components/MovieCard.css";
 
 function MovieCard({ movie }) {
   return (
     <div className="movie-card">
-      <img src={movie.poster_path} alt={movie.title} />
+      <Link to={`/movies/${movie.id}`}>
+        <img src={movie.poster_path} alt={movie.title} />
+      </Link>
     </div>
   );
 }
@@ -14,6 +16,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     poster_path: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
   }).isRequired,
 };
 
