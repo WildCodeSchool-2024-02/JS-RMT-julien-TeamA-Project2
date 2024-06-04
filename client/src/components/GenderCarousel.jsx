3290a51b85f2b1345bba4aae5b54c4bea8ee55a2
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-04 08:50:27 +0200

    desktop style

--- client/src/components/GenderCarousel.jsx
+++ client/src/components/GenderCarousel.jsx
@@ -1,22 +1,27 @@
 import PropTypes from "prop-types";
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-
+import '../assets/styles/components/GenderCarousel.css'
 
 function GenderCarousel({similarMovies}) {
-  const items = similarMovies.map((movie) => ( <img src={movie.poster_path} alt={movie.title} key={movie.poster_path}/> ));
+  const items = similarMovies.map((movie) => ( <img className='carousel-img' src={movie.poster_path} alt={movie.title} key={movie.poster_path}/> ));
   const responsive = {
     0: { items: 1 },
-    568: { items: 2 },
-    1024: { items: 3 },
+    568: { items: 3 },
+    1024: { items: 7 },
 };
   return(
+    <div className="carousel-container">
+      <h2>You would also like :</h2>
       <AliceCarousel
-          mouseTracking
+          infinite
           items={items}
           responsive={responsive}
           controlsStrategy="alternate"
-      />
+          renderNextButton={() => <button type="button" className="next-button">{">"}</button>}
+          renderPrevButton={() => <button type="button" className="prev-button">{"<"}</button>}
+          />
+    </div>
   )
 }
 
