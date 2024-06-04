5e424b4c3e058724ac2e35f9ebf5c6623bfbbd69
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-04 12:28:23 +0200

    remove caroussel if no similar movies

--- client/src/components/GenderCarousel.jsx
+++ client/src/components/GenderCarousel.jsx
@@ -2,23 +2,26 @@ import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import '../assets/styles/components/GenderCarousel.css'
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "../assets/styles/components/GenderCarousel.css";
 
 function GenderCarousel({ genre, movieId }) {
-  const [moviesByGenre, setMoviesByGenre] = useState([])
+  const [moviesByGenre, setMoviesByGenre] = useState([]);
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}/api/movies?genre=${genre}&id=${movieId}`)
-      .then(res => setMoviesByGenre(res.data))
-      .catch(err => console.error(err));
-      // eslint-disable-next-line
+    axios
+      .get(
+        `${import.meta.env.VITE_API_URL}/api/movies?genre=${genre}&id=${movieId}`
+      )
+      .then((res) => setMoviesByGenre(res.data))
+      .catch((err) => console.error(err));
+    // eslint-disable-next-line
   }, [movieId]);
 
   const items = moviesByGenre.map((movie) => (
     <Link to={`/movies/${movie.id}`} key={movie.id}>
-      <img className='carousel-img' src={movie.poster_path} alt={movie.title}/> 
+      <img className="carousel-img" src={movie.poster_path} alt={movie.title} />
     </Link>
   ));
 
@@ -26,26 +29,35 @@ function GenderCarousel({ genre, movieId }) {
     0: { items: 2 },
     700: { items: 3 },
     1024: { items: 6 },
-};
-  return(
+  };
+
+  if (items.length === 0) return "";
+  return (
     <div className="carousel-container">
       <h2>You would also like :</h2>
       <AliceCarousel
-          infinite
-          items={items}
-          responsive={responsive}
-          controlsStrategy="alternate"
-          renderNextButton={() => <button type="button" className="next-button">{">"}</button>}
-          renderPrevButton={() => <button type="button" className="prev-button">{"<"}</button>}
-          />
+        infinite
+        items={items}
+        responsive={responsive}
+        controlsStrategy="alternate"
+        renderNextButton={() => (
+          <button type="button" className="next-button">
+            {">"}
+          </button>
+        )}
+        renderPrevButton={() => (
+          <button type="button" className="prev-button">
+            {"<"}
+          </button>
+        )}
+      />
     </div>
-  )
+  );
 }
 
 GenderCarousel.propTypes = {
   genre: PropTypes.string.isRequired,
-  movieId: PropTypes.number.isRequired
-}
+  movieId: PropTypes.number.isRequired,
+};
 
 export default GenderCarousel;
-
