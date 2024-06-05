0035abffc7434a1e5fcefa9ea9e996af1f04f1b6
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-05 12:04:42 +0200

    add navigation componants to ErrorPage

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,6 +1,16 @@
-function ErrorPage() {
-  return  <h1 style={{color : "white"}}> We can't find that movie </h1>
-}
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
+import "../assets/styles/pages/ErrorPage.css";
+
+function ErrorPage() {
+  return (
+    <>
+      <Navigation />
+      <h1 className="error-message"> Oops ! An error as ocurred ... </h1>
+      <Footer />
+    </>
+  );
+}
 
 export default ErrorPage;
